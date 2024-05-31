e11cb7d2649d98480a857b60d6fed8ee8dc9cfc3
Pragathiswaran <mpragathiswaran@gmail.com>
2024-05-31 10:46:38 +0530

    commit

--- dom/app.js
+++ dom/app.js
@@ -1,8 +1,25 @@
-const paragraph = document.querySelector("#root");
-const buttons =  document.querySelector("#btn");
+const number = document.querySelector("h1")
+const increase = document.querySelector(".btn-increase")
+const decrease = document.querySelector(".btn-decrease")
 
-buttons.addEventListener("click",() =>{
-    if(paragraph!== null){
-        paragraph.innerHTML = "SUCCESS";
-    }
-});
+var count = 0
+
+increase.addEventListener("click", function(){
+    count++
+    number.textContent = count
+})
+
+decrease.addEventListener("click", function(){
+    count = count <= 0 ? 0 : count -1
+    number.textContent = count
+})
+
+document.addEventListener('DOMContentLoaded', ()=>{
+    count = 0 
+    number.textContent = count
+})
+
+setInterval(()=>{
+    count = count < 10 ? count - 1 : 0
+    number.textContent = count
+}, 1000)
---
+++ practice/practice.js
@@ -0,0 +1,11 @@
+var total = 10
+
+var sum = function(num1, num2){
+    console.log(total)
+
+    var total
+    total = num1 + num2 
+   return total
+}
+
+console.log(sum(10, 20))
