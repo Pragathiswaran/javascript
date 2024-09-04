572a3c575a15f1473955ab3b68fe7891801c1b08
Pragathiswaran <mpragathiswaran@gmail.com>
2024-09-04 22:58:19 +0530

    feat : created the slider app

---
+++ slider/app.js
@@ -0,0 +1,29 @@
+const slide = document.querySelectorAll('.slide')
+const prevButton = document.querySelector('.prev-btn')
+const nextButton = document.querySelector('.next-btn')
+let count = 0;
+
+nextButton.addEventListener('click',()=>{
+
+    if(count < slide.length - 1){
+        slide[count].classList.remove('active')
+        count++
+        slide[count].classList.add('active')
+    } else {
+        slide[count].classList.remove('active')
+        count = 0
+        slide[count].classList.add('active')
+    }
+})
+
+prevButton.addEventListener('click',()=>{
+    if(count > 0){
+        slide[count].classList.remove('active')
+        count--
+        slide[count].classList.add('active')
+    } else {
+        slide[count].classList.remove('active')
+        count = slide.length - 1
+        slide[count].classList.add('active')
+    }
+})
