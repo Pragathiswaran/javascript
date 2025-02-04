1ee76f4d396dec6282d113b87b13ddb521c1473e
Pragathiswaran <mpragathiswaran@gmail.com>
2025-02-04 22:22:16 +0530

    practice

--- practice/jsMethods.js
+++ practice/jsMethods.js
@@ -191,3 +191,125 @@ function findDuplicates(arr) {
  console.log(arrayIntersection([1, 2, 3, 4], [3, 4, 5, 6])); 
 
  
+ function findLargestNumber(arr) {
+    if(arr.length === 0) return 0
+    let res = arr[0]
+    for(let value of arr){
+       if(value > res){
+           res = value
+       }
+    }
+    return res
+  }
+  console.log(findLargestNumber([10, 20, 5, 30, 15])); // Output: 30
+
+  function isPrime(num) {
+    if(num <= 1) return false
+    if(num === 2) return true
+    if(num % 2 === 0) return false
+    
+   for(let i = 3; i <= Math.sqrt(num); i+=2){
+       if(num % i === 0) return false
+   }
+    
+    return true
+  }
+  console.log(isPrime(11)); 
+  console.log(isPrime(337));
+
+
+  function findMissingNumber(arr) {
+    if(arr.length === 0) return undefined
+    let n = arr.length + 1
+    let expectedSum = (n * (n + 1)) / 2
+    let actualSum = arr.reduce((sum,num) => sum + num, 0)
+    return expectedSum - actualSum
+  }
+  console.log(findMissingNumber([1, 2, 3, 5])); // Output: 4
+  
+  const arr = [1, 2, 3, 5]
+  const result = arr.reduce((sum,num) => sum + num, 0)
+  console.log(result)
+
+
+  function isPalindrome(str) {
+    let left = 0, right = str.length - 1
+    
+    while(left < right){
+        if(str[left] !== str[right]){
+            return false
+        }
+        left++
+        right--
+    }
+    return true
+}
+console.log(isPalindrome("madam")); // Output: true
+console.log(isPalindrome("hello")); // Output: false
+
+
+function factorial(num) {
+    if(num <= 1) return num
+    
+    return num * factorial(num - 1)
+  }
+  console.log(factorial(5)); 
+
+function secondLargest(arr) {
+    if(arr.length < 2) return "The Array must have atleast two element"
+    firstMax = -Infinity
+    secondMax = -Infinity
+    
+    for(let value of arr){
+        if(value > firstMax){
+            secondMax = firstMax
+            firstMax = value
+        } else if (value > secondMax && value < firstMax){
+            secondMax = value
+        }
+    }
+    
+    if(secondMax === -Infinity) return "All elements are same"
+    
+    return secondMax
+}
+console.log(secondLargest([10, 20, 5, 30, 23, 15])); // Output: 23
+console.log(secondLargest([10,10,10,10])); // Output: All the elements are same
+console.log(secondLargest([10])); // Output: The Array must have atleast two element
+
+
+function countChar(str, char) {
+    if(typeof char !== "string" || char.length < 1) return "Invalid Character"
+    let count = 0
+    for(let value of str){
+       if(value == char) count++
+    }
+    return count
+  }
+  console.log(countChar("hello world", "l")); // Output: 3
+
+  function areArraysEqual(arr1, arr2) {
+    if(arr1.length !== arr2.length) return false
+    
+    for(let i = 0; i < arr1.length; i++){
+        if(arr1[i] !== arr2[i]) return false
+    }
+    return true
+  }
+  console.log(areArraysEqual([1, 2, 3], [1, 2, 3])); // Output: true
+  console.log(areArraysEqual([1, 2, 3], [1, 3, 2])); // Output: false
+
+function removeDuplicates(arr) {
+    return Array.from(new Set(arr))
+}
+console.log(removeDuplicates([1, 2, 2, 3, 4, 4, 5])); // Output: [1, 2, 3, 4, 5]
+
+
+
+function rotateArray(arr, k) {
+    let n = arr.length;
+    k = k % n
+    return [...arr.slice(-k),...arr.slice(0,n -k)]
+}
+console.log(rotateArray([1, 2, 3, 4, 5, 6], 2));
+console.log(1 % 5)
